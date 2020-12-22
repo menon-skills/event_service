63e5aaa33c5703d0ca8d8747f46b1b7d103b0714
Mike <michael.moor@sbb.ch>
2020-12-22 14:40:00 +0100

    Reformat with dartfmt -w

--- lib/src/local/local_event_service.dart
+++ lib/src/local/local_event_service.dart
@@ -6,14 +6,17 @@ import '../event_service.dart';
 class LocalEventService implements EventService {
   final StreamController _eventController;
 
-  LocalEventService({bool sync = false}) : _eventController = StreamController.broadcast(sync: sync);
+  LocalEventService({bool sync = false})
+      : _eventController = StreamController.broadcast(sync: sync);
 
-  LocalEventService.customController(StreamController controller) : _eventController = controller;
+  LocalEventService.customController(StreamController controller)
+      : _eventController = controller;
 
   @override
   Stream<T> listen<T>({EventFilter filter}) {
     return _eventController.stream
-        .where((event) => event is T && (filter != null ? filter.filter(event) : true))
+        .where((event) =>
+            event is T && (filter != null ? filter.filter(event) : true))
         .cast<T>();
   }
 
